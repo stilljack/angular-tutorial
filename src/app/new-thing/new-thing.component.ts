5a02daf5d1cd73e376be044cda653ab568e5f475
stilljack <stilljack@gmail.com>
2020-02-22 20:26:21 -0800

    3 files modified

--- src/app/new-thing/new-thing.component.ts
+++ src/app/new-thing/new-thing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-new-thing',
   templateUrl: './new-thing.component.html',
@@ -7,6 +8,7 @@ import { Input } from '@angular/core';
 })
 export class NewThingComponent implements OnInit {
   @Input() product;
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
